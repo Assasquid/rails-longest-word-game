1fdc9c883aa4da1b8aef15937f78d710bc49b042
Jerome Million Rousseau <jerome@nebula-tales.com>
2023-02-20 17:03:49 +0000

    Getting 10 randoms letters.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def new
+    alphabet = ('a'..'z').to_a
+    alphabet_shuffled = alphabet.shuffle
+    @letters = alphabet_shuffled.slice(0, 9)
+  end
 end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def new
+    @letters = []
+  end
+
+  def score
+  end
+end
